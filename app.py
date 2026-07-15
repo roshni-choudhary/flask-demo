50ba29f7a4d2054f3072bbd0ce7ed765474c6378
roshni-choudhary <roshnichoudhary2308@gmail.com>
2026-07-15 12:39:51 +0000

    Add Flask web UI assets

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+    # return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
  app.run(debug=True)
