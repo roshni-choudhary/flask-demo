ea6521ae26f579e43fe7bcfe258b4b4f1e182224
roshni-choudhary <roshnichoudhary2308@gmail.com>
2026-07-15 14:19:33 +0000

    Polish Flask demo

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask,render_template
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
 @app.route("/")
 def hello_world():
